e74a65af3921a800889fcb5e57557727cffa4ef3
Dry2000 <e1625@s.akashi.ac.jp>
2021-06-22 16:40:32 +0900

    delete incorrect folder

--- recipeReseacher/Pods/Target Support Files/Pods-recipeResearcher-recipeResearcherUITests/Pods-recipeResearcher-recipeResearcherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_recipeResearcher_recipeResearcherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_recipeResearcher_recipeResearcherUITests
-@end
--- recipeReseacher/Pods/Target Support Files/Pods-recipeResearcherTests/Pods-recipeResearcherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_recipeResearcherTests : NSObject
-@end
-@implementation PodsDummy_Pods_recipeResearcherTests
-@end
